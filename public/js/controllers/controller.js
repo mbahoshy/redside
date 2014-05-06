1e0c1eb90331ab9a4b5ca866edcbfe703a9628f4
Matt <mbahoshy@gmail.com>
2014-05-06 16:49:33 -0700

    added map pan functionality, stylized map pane. allow switching from detail view to listings view

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -16,7 +16,9 @@ redSide.controller("mapController", function ($scope, $routeParams, $http) {
 			price = ($routeParams.price),
 			coord,
 			map,
+			toggleCounter = 0,
 			listings;
+
 
 			var infowindow = new google.maps.InfoWindow({
 			  content: "hello"
@@ -25,10 +27,20 @@ redSide.controller("mapController", function ($scope, $routeParams, $http) {
 		$http.get('/mapListings/' + neighborhood + '/' + size + '/' + price).success(function(data, status){
 
 			listings = data;
+			$scope.listings = listings;
 			// $scope.data = data;
 		    var mapOptions = {
 	          center: new google.maps.LatLng(47.6323268, -122.3568641),
-	          zoom: 14
+	          zoom: 14,
+	          zoomControl: true,
+			  zoomControlOptions: {
+			  		style: google.maps.ZoomControlStyle.LARGE,
+					position: google.maps.ControlPosition.RIGHT_CENTER
+			  },
+			  panControl: true,
+			  panControlOptions: {
+			  		position: google.maps.ControlPosition.TOP_RIGHT
+			  },
 	        };
 	        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 	        setMarkers();
@@ -52,8 +64,9 @@ redSide.controller("mapController", function ($scope, $routeParams, $http) {
 
 						}
 					}
-					toggleShortlistingContainer (false);
-					
+					$scope.toggleShortlistingContainer (true);
+					$scope.toggleTabs(1);
+					map.panTo(this.position);
 				});
 
 				marker.setMap(map);
@@ -62,22 +75,36 @@ redSide.controller("mapController", function ($scope, $routeParams, $http) {
 
 		}
 		
-		function toggleShortlistingContainer (close) {
-
-			var container = $('.map-sidebar');
+		$scope.toggleShortlistingContainer = function (mapPoint) {
 			var sl = $('.shortlisting-container');
 			var tab = $('.map-sidebar-button');
-			var width = container.css('width');
-			console.log(width);
-			if (close == true) {
-				// container.animate({width:'0'}, 250);
+			if (toggleCounter === 0) {
+				tab.animate({left:"25%"}, 250);
+				sl.animate({left:'0'}, 250);
+				toggleCounter = 1;		
 			}
-			// container.animate({width:'25%'}, 250);
-			tab.animate({left:"25%"}, 250);
-			sl.animate({left:'0'}, 250, function () {
-				$('.shortlisting-container p').animate({opacity:'1'}, 250);
-			});
-			console.log(width);
+			else if (mapPoint != true) {
+				toggleCounter = 0;
+				tab.animate({left:"0"}, 250);				
+				sl.animate({left:"-102%"}, 250);
+			} 
+
+		}
+
+		$scope.toggleTabs = function (toggle) {
+			if (toggle == 0) {
+				$('#listing_tab').css({display:'visible'});
+				$('#detail_tab').css({display:'none'});
+
+				$('.listing-tab').addClass('active-tab');
+				$('.detail-tab').removeClass('active-tab');
+			} else {
+				$('#listing_tab').css({display:'none'});
+				$('#detail_tab').css({display:'visible'});		
+
+				$('.listing-tab').removeClass('active-tab');
+				$('.detail-tab').addClass('active-tab');		
+			}
 		}
 
 });
